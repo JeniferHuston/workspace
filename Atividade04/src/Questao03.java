936ad118f3794ddc2b16b6db328bdb7a3492679e
Jenifer Huston <jenifer.oliveira@catolica.edu.br>
2021-06-09 22:14:36 -0300

    Última versão do ercercício 3 usando JOptionPane.

--- Atividade04/src/Questao03.java
+++ Atividade04/src/Questao03.java
@@ -8,14 +8,10 @@ public class Questao03 {
 		// TODO Auto-generated method stub
 		
 		int num1, num2, maior, menor;
-		//String str = "";
+		StringBuffer str = new StringBuffer();
 		Scanner scan = new Scanner(System.in);
-		//num1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o primeiro número: "));
-		//num2 = Integer.parseInt(JOptionPane.showInputDialog("Informe o segundo número: "));
-		System.out.println("Informe o primeiro número: ");
-		num1 = scan.nextInt();
-		System.out.println("Informe o segundo número: ");
-		num2 = scan.nextInt();
+		num1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o primeiro número: "));
+		num2 = Integer.parseInt(JOptionPane.showInputDialog("Informe o segundo número: "));
 		
 		if (num1 > num2) {
 			maior = num1;
@@ -26,17 +22,12 @@ public class Questao03 {
 		}
 		
 		if (maior == menor) {
-			//JOptionPane.showMessageDialog(null, "Os números são iguais.");
-			System.out.println("Os números são iguais.");
+			JOptionPane.showMessageDialog(null, "Os números são iguais.");
 		} else {
-			//JOptionPane.showMessageDialog(null, "Os numeros entre "+menor+" e "+maior+" são: ");
-			System.out.println("Os numeros entre "+menor+" e "+maior+" são: ");
 			for (int i = menor+1; i < maior; i++) {
-				//str.concat(String.valueOf(i)+";");
-				System.out.print(i+"; ");
+				str.append(String.valueOf(i)+"; ");
 			}
-			//System.out.println(str);
-			//JOptionPane.showMessageDialog(null, str);
+			JOptionPane.showMessageDialog(null, "Os numeros entre "+menor+" e "+maior+" são: "+str);
 		}
 		scan.close();	
 	}
